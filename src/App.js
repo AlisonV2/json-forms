5eaa54bb4c30c1c43039ea9d5ea7c8a186de6a1f
Alison Vdr <alison.vandromme007@gmail.com>
2021-06-09 11:15:10 +0200

    Deleted unused files and moved json to its own folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-import Data from "./data.json";
+import Data from "./data/data.json";
 import CustomForm from "./components/custom-form.component";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <CustomForm {...Data} />
     </div>
   );
