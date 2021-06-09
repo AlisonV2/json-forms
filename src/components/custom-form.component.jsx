9d1118326677bfd291002a35759a59574b144d42
Alison Vdr <alison.vandromme007@gmail.com>
2021-06-09 11:09:10 +0200

    Migrated styles to their own folder

--- src/components/custom-form.component.jsx
+++ src/components/custom-form.component.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
 
-import {CustomButton, CustomButtonContainer} from "./custom-button.styles";
-import {FormContainer} from "./custom-form.styles";
+import {CustomButton, CustomButtonContainer} from "../styles/custom-button.styles";
+import {FormContainer} from "../styles/custom-form.styles";
 
 const CustomForm = ({ email, options }) => {
-  console.log(email, options);
   return (
     <FormContainer>
       <Form action="/form" method="post" className="col-5">
-          <Form.Group controlId="formBasicEmail" className="mb-3">
-              <Form.Control type="email" id="email" placeholder="Votre adresse e-mail" />
+          <Form.Group className="mb-3">
+              <Form.Control type="email" name={email} id={email} placeholder="Votre adresse e-mail" />
           </Form.Group>
-          <Form.Group>
-            <Form.Control as="select" name="services" id="services" className="mb-3">
-                  <option value=""> Votre service</option>
+          <Form.Group className="mb-3">
+            <Form.Control as="select" name="services" id="services">
+                  <option value="" disabled selected hidden> Votre service</option>
                 {options.map((option, index) => (
                   <option value={option} key={index}>
                     {option}
